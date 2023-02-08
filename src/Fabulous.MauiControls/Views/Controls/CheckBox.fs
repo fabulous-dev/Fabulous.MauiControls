a13410ab978b17c7df2215e0f6a20813c17e308c
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2023-02-08 11:59:27 +0100

    Clean the simple Control widgets

--- src/Fabulous.MauiControls/Views/Controls/CheckBox.fs
+++ src/Fabulous.MauiControls/Views/Controls/CheckBox.fs
@@ -3,15 +3,17 @@ namespace Fabulous.Maui
 open System.Runtime.CompilerServices
 open Fabulous
 open Microsoft.Maui.Controls
+open Microsoft.Maui.Graphics
 
 type IFabCheckBox =
     inherit IFabView
 
 module CheckBox =
-
     let WidgetKey = Widgets.register<CheckBox>()
 
-    let Color = Attributes.defineBindableAppThemeColor CheckBox.ColorProperty
+    let Color = Attributes.defineBindableWithEquality CheckBox.ColorProperty
+    
+    let FabColor = Attributes.defineBindableWithEquality CheckBox.ColorProperty
 
     let IsCheckedWithEvent =
         Attributes.defineBindableWithEvent "CheckBox_CheckedChanged" CheckBox.IsCheckedProperty (fun target -> (target :?> CheckBox).CheckedChanged)
@@ -19,7 +21,9 @@ module CheckBox =
 [<AutoOpen>]
 module CheckBoxBuilders =
     type Fabulous.Maui.View with
-
+        /// <summary>Create a CheckBox widget with a state and listen for state changes</summary>
+        /// <param name="isChecked">The state of the checkbox</param>
+        /// <param name="onCheckedChanged">Message to dispatch</param>
         static member inline CheckBox<'msg>(isChecked: bool, onCheckedChanged: bool -> 'msg) =
             WidgetBuilder<'msg, IFabCheckBox>(
                 CheckBox.WidgetKey,
@@ -28,14 +32,23 @@ module CheckBoxBuilders =
 
 [<Extension>]
 type CheckBoxModifiers =
-    /// <summary>Set the color of the checkBox.</summary>
-    /// <param name="light">The color of the checkBox in the light theme.</param>
-    /// <param name="dark">The color of the checkBox in the dark theme.</param>
+    /// <summary>Set the color of the check box</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="value">The color of the check box</param>
     [<Extension>]
-    static member inline color(this: WidgetBuilder<'msg, #IFabCheckBox>, light: FabColor, ?dark: FabColor) =
-        this.AddScalar(CheckBox.Color.WithValue(AppTheme.create light dark))
+    static member inline color(this: WidgetBuilder<'msg, #IFabCheckBox>, value: Color) =
+        this.AddScalar(CheckBox.Color.WithValue(value))
+        
+    /// <summary>Set the color of the check box</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="value">The color of the check box</param>
+    [<Extension>]
+    static member inline color(this: WidgetBuilder<'msg, #IFabCheckBox>, value: FabColor) =
+        this.AddScalar(CheckBox.FabColor.WithValue(value))
 
     /// <summary>Link a ViewRef to access the direct CheckBox control instance</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="value">The ViewRef instance that will receive access to the underlying control</param>
     [<Extension>]
     static member inline reference(this: WidgetBuilder<'msg, IFabCheckBox>, value: ViewRef<CheckBox>) =
         this.AddScalar(ViewRefAttributes.ViewRef.WithValue(value.Unbox))
--- src/Fabulous.MauiControls/Views/Controls/Switch.fs
+++ src/Fabulous.MauiControls/Views/Controls/Switch.fs
@@ -3,6 +3,7 @@ namespace Fabulous.Maui
 open System.Runtime.CompilerServices
 open Fabulous
 open Microsoft.Maui.Controls
+open Microsoft.Maui.Graphics
 
 type IFabSwitch =
     inherit IFabView
@@ -10,9 +11,13 @@ type IFabSwitch =
 module Switch =
     let WidgetKey = Widgets.register<Switch>()
 
-    let ColorOn = Attributes.defineBindableAppThemeColor Switch.OnColorProperty
+    let ColorOn = Attributes.defineBindableWithEquality Switch.OnColorProperty
+    
+    let FabColorOn = Attributes.defineBindableColor Switch.OnColorProperty
 
-    let ThumbColor = Attributes.defineBindableAppThemeColor Switch.ThumbColorProperty
+    let ThumbColor = Attributes.defineBindableWithEquality Switch.ThumbColorProperty
+    
+    let ThumbFabColor = Attributes.defineBindableColor Switch.ThumbColorProperty
 
     let IsToggledWithEvent =
         Attributes.defineBindableWithEvent "Switch_Toggled" Switch.IsToggledProperty (fun target -> (target :?> Switch).Toggled)
@@ -20,7 +25,9 @@ module Switch =
 [<AutoOpen>]
 module SwitchBuilders =
     type Fabulous.Maui.View with
-
+        /// <summary>Create a Switch widget with a toggle state and listen for toggle state changes</summary>
+        /// <param name="isToggled">The toggle state</param>
+        /// <param name="onToggled">Message to dispatch</param>
         static member inline Switch<'msg>(isToggled: bool, onToggled: bool -> 'msg) =
             WidgetBuilder<'msg, IFabSwitch>(
                 Switch.WidgetKey,
@@ -29,21 +36,37 @@ module SwitchBuilders =
 
 [<Extension>]
 type SwitchModifiers =
-    /// <summary>Set the color of the thumbColor.</summary>
-    /// <param name="light">The color of the thumbColor in the light theme.</param>
-    /// <param name="dark">The color of the thumbColor in the dark theme.</param>
+    /// <summary>Set the color of the on state</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="value">The color of the on state</param>
     [<Extension>]
-    static member inline thumbColor(this: WidgetBuilder<'msg, #IFabSwitch>, light: FabColor, ?dark: FabColor) =
-        this.AddScalar(Switch.ThumbColor.WithValue(AppTheme.create light dark))
+    static member inline colorOn(this: WidgetBuilder<'msg, #IFabSwitch>, value: Color) =
+        this.AddScalar(Switch.ColorOn.WithValue(value))
 
-    /// <summary>Set the color of the on state.</summary>
-    /// <param name="light">The color of the on state in the light theme.</param>
-    /// <param name="dark">The color of the on state in the dark theme.</param>
+    /// <summary>Set the color of the on state</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="value">The color of the on state</param>
     [<Extension>]
-    static member inline colorOn(this: WidgetBuilder<'msg, #IFabSwitch>, light: FabColor, ?dark: FabColor) =
-        this.AddScalar(Switch.ColorOn.WithValue(AppTheme.create light dark))
+    static member inline colorOn(this: WidgetBuilder<'msg, #IFabSwitch>, value: FabColor) =
+        this.AddScalar(Switch.FabColorOn.WithValue(value))
+        
+    /// <summary>Set the color of the thumb</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="value">The color of the thumb</param>
+    [<Extension>]
+    static member inline thumbColor(this: WidgetBuilder<'msg, #IFabSwitch>, value: Color) =
+        this.AddScalar(Switch.ThumbColor.WithValue(value))
+        
+    /// <summary>Set the color of the thumb</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="value">The color of the thumb</param>
+    [<Extension>]
+    static member inline thumbColor(this: WidgetBuilder<'msg, #IFabSwitch>, value: FabColor) =
+        this.AddScalar(Switch.ThumbFabColor.WithValue(value))
 
     /// <summary>Link a ViewRef to access the direct Switch control instance</summary>
+    /// <param name="this">Current widget</param>
+    /// <param name="value">The ViewRef instance that will receive access to the underlying control</param>
     [<Extension>]
     static member inline reference(this: WidgetBuilder<'msg, IFabSwitch>, value: ViewRef<Switch>) =
         this.AddScalar(ViewRefAttributes.ViewRef.WithValue(value.Unbox))
