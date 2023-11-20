2f939dabb536c86311db17881bb0ed3f81fe9d32
Timothé Larivière <timothe.lariviere@outlook.com>
2023-11-20 14:03:25 +0100

    Improve State<'T>

--- src/Fabulous.MauiControls/Component.fs
+++ src/Fabulous.MauiControls/Component.fs
@@ -1,6 +1,7 @@
 namespace Fabulous.Maui
 
 open System
+open System.ComponentModel
 open System.Runtime.CompilerServices
 open Fabulous
 open System.Collections.Generic
@@ -154,11 +155,33 @@ module ComponentBuilders =
             )
             
 ////////////// State //////////////
+
 type StateRequest<'T> = delegate of unit -> 'T
 
-type [<Struct>] State<'T>(ctx: Context, key: int, value: 'T) =
-    member this.Current = value
-    member this.Set(value: 'T) = ctx.SetValue(key, value)
+/// DESIGN: State<'T> is meant to be very short lived.
+/// It is created on Bind (let!) and destroyed at the end of a single ViewBuilder CE execution.
+/// Due to its nature, it is very likely it will be captured by a closure and allocated to the memory heap when it's not needed.
+/// e.g. Button("Increment", fun () -> state.Set(state.Current + 1))
+///
+/// The Set method is therefore marked inlinable to avoid creating a closure capturing State<'T>
+/// The compiler will rewrite the lambda as follow:
+/// Button("Increment", fun () -> ctx.SetValue(key, current + 1))
+///
+/// One constraint of inlining is to have all used fields public: Context, Key, Current
+/// But we don't wish to expose the Context and Key fields to the user, so we mark them as EditorBrowsable.Never
+type [<Struct>] State<'T>=
+    [<EditorBrowsable(EditorBrowsableState.Never)>]
+    val public Context: Context
+    
+    [<EditorBrowsable(EditorBrowsableState.Never)>]
+    val public Key: int
+    
+    val public Current: 'T
+    
+    new (ctx, key, value) = { Context = ctx; Key = key; Current = value }
+    
+    member inline this.Set(value: 'T) =
+        this.Context.SetValue(this.Key, value)
 
 [<Extension>]
 type StateExtensions =
