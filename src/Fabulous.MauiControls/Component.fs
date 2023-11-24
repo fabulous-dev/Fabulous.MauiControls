a5a74a16be610f76b024ee5d5bf27c985ac9e7c2
Timothé Larivière <timothe.lariviere@outlook.com>
2023-11-24 13:45:07 +0100

    Argument for MvuComponent

--- src/Fabulous.MauiControls/Component.fs
+++ src/Fabulous.MauiControls/Component.fs
@@ -18,4 +18,6 @@ module ComponentBuilders =
     type Fabulous.Maui.View with
         static member inline Component<'marker>() = ComponentBuilder()
         
-        static member MvuComponent(program) = MvuComponentBuilder(program)
+        static member inline MvuComponent(program: Program<unit, 'model, 'msg>) = MvuComponentBuilder(program, ())
+        
+        static member inline MvuComponent(program: Program<'arg, 'model, 'msg>, arg: 'arg) = MvuComponentBuilder(program, arg)
