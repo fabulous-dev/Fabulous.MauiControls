1333235d6425404b29816b431c9a849988622692
Timothé Larivière <timothe.lariviere@kanoyatech.com>
2024-01-18 15:35:05 +0100

    Use Fabulous 2.5.0-pre4

--- src/Fabulous.MauiControls/Component.fs
+++ src/Fabulous.MauiControls/Component.fs
@@ -17,8 +17,10 @@ module Component =
 module ComponentBuilders =
     type Fabulous.Maui.View with
 
-        static member inline Component<'msg, 'marker>() = ComponentBuilder()
+        static member inline Component<'msg, 'marker>() = ComponentBuilder<'msg>()
 
-        static member inline Component(program: Program<unit, 'model, 'msg>) = MvuComponentBuilder(program, ())
+        static member inline Component<'msg, 'model, 'marker, 'parentMsg>(program: Program<unit, 'model, 'msg>) =
+            MvuComponentBuilder<unit, 'msg, 'model, 'marker, 'parentMsg>(program, ())
 
-        static member inline Component(program: Program<'arg, 'model, 'msg>, arg: 'arg) = MvuComponentBuilder(program, arg)
+        static member inline Component<'arg, 'msg, 'model, 'marker, 'parentMsg>(program: Program<'arg, 'model, 'msg>, arg: 'arg) =
+            MvuComponentBuilder<'arg, 'msg, 'model, 'marker, 'parentMsg>(program, arg)
